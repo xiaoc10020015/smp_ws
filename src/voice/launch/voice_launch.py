7679979be2fc7721d916385cdd74a8c5e22fe760
xiaoc10020015 <206500094@qq.com>
2022-09-04 16:48:00 +0800

    voice launch add

---
+++ src/voice/launch/voice_launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription           
+from launch_ros.actions import Node            
+
+def generate_launch_description(): 
+    voice_pkg_name = 'voice'
+
+    return LaunchDescription([                 
+        Node(                                  
+            package=voice_pkg_name,          
+            executable='iat_publish',
+            namespace='qianxun',       
+            name='iat_publish',                 
+        ),
+        Node(                                  
+            package=voice_pkg_name,          
+            executable='tts_subscribe',
+            namespace='qianxun',       
+            name='tts_subscribe',       
+        )
+    ])
